e7007baa749f0398bfb5ccbf72ac0cddf7fee65b
hmon <wtthumon@gmail.com>
2024-12-05 05:25:40 +0800

    day four solution

--- src/DayFour.java
+++ src/DayFour.java
@@ -1,4 +1,103 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DayFour {
+    public static int findXMAS(ArrayList<String> matrix) {
+        int total = 0;
+        for (String each : matrix) {
+            String reversed = new StringBuilder(each).reverse().toString();
+            total+=DayThree.countMatches("XMAS", each);
+            total+=DayThree.countMatches("XMAS", reversed);
+            //if (DayThree.findMatchBoolean("SAMX",reversed)) total+=1;
+        }
+
+        return total;
+    }
+    public static String reverseString(String originalString){
+        String reversedString = "";
+        for (int i = originalString.length()-1; i >=0; i--){
+            reversedString+=originalString.charAt(i);
+        }
+        return reversedString;
+    }
+
+   public static ArrayList<String> swapRowsAndCols (ArrayList<String> matrix) {
+       ArrayList<String> transformedMatrix = new ArrayList<>();
+       String newRow = "";
+       for (int i = 0; i < matrix.size(); i++) {
+           for (int j = 0; j < (matrix.get(0)).length(); j++) {
+               newRow += matrix.get(j).charAt(i);
+           }
+           transformedMatrix.add(newRow);
+           newRow = "";
+       }
+       return transformedMatrix;
+   }
+
+
+   public static ArrayList<String> diagnoalise(ArrayList<String> matrix){
+        ArrayList<String> diagonalList = new ArrayList<>();
+        String diagonals = "";
+        int rowLimit = matrix.size();
+        int colLimit = matrix.get(0).length();
+        int currentRow = 0;
+        int currentCol = 0;
+        boolean goingUp = true;
+        int limit = rowLimit * colLimit;
+        int counter = 0;
+       while (counter != limit){
+           if (goingUp) {
+               while (currentRow >= 0 && currentCol < colLimit) {
+                   char charAtCurrentPosition = matrix.get(currentRow).charAt(currentCol);
+                   diagonals += charAtCurrentPosition;
+                   currentRow -= 1;
+                   currentCol += 1;
+                   counter+=1;
+               }
+               if (currentCol == colLimit) {
+                   currentCol -= 1;
+                   currentRow += 2;
+
+               } else {
+                   currentRow += 1;
+               }
+               diagonalList.add(diagonals);
+               diagonals = "";
+           }
+           goingUp = false;
+           if (!goingUp) {
+               while (currentRow < rowLimit && currentCol >= 0) {
+                   char charAtCurrentPosition = matrix.get(currentRow).charAt(currentCol);
+                   diagonals += charAtCurrentPosition;
+                   currentRow += 1;
+                   currentCol -= 1;
+
+                   counter+=1;
+               }
+               if (currentRow == rowLimit) {
+                   currentRow -= 1;
+                   currentCol += 2;
+               } else {
+                   currentCol += 1;
+               }
+               diagonalList.add(diagonals);
+               diagonals = "";
+           }
+           goingUp = true;
+       }
+       return diagonalList;
+   }
+   public static ArrayList<String> reverse(ArrayList<String> input){
+        ArrayList<String> output = new ArrayList<>();
+        for (String each: input){
+            String reversed = new StringBuilder(each).reverse().toString();
+            output.add(reversed);
+        }
+        return output;
+   }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,19 @@ public class Main {
         ReadFromFileUsingScanner scanner = new ReadFromFileUsingScanner();
 
         //////-------------------------------------DAY ONE------------------------------------------------////////
-        inputDownloader.downloadInput("https://adventofcode.com/2024/day/1/input","dayOneInput");
-        ArrayList<String> dayOneInputRaw = scanner.load("dayOneInput");
+        inputDownloader.downloadInput("https://adventofcode.com/2024/day/1/input", "dayOneInput");
+        ArrayList<String> dayOneInputRaw = scanner.loadAsArray("dayOneInput");
         LeftAndRightComparison dayOneInputCleaned = new LeftAndRightComparison(dayOneInputRaw);
         int partOneTotal = dayOneInputCleaned.getTotalDifference();
         //part-one
-        System.out.println("Part one answer: "+partOneTotal);
+        System.out.println("Part one answer: " + partOneTotal);
         //part-two
         Dictionary rightAppearsInTheLeft = new Dictionary(dayOneInputCleaned);
-        System.out.println("Part two answer: "+ rightAppearsInTheLeft.multiplyKeysAndValues());
+        System.out.println("Part two answer: " + rightAppearsInTheLeft.multiplyKeysAndValues());
 
         //////-------------------------------------DAY TWO------------------------------------------------////////
-        inputDownloader.downloadInput("https://adventofcode.com/2024/day/2/input","dayTwoInput");
-        ArrayList<String> dayTwoInputRaw = scanner.load("dayTwoInput");
+        inputDownloader.downloadInput("https://adventofcode.com/2024/day/2/input", "dayTwoInput");
+        ArrayList<String> dayTwoInputRaw = scanner.loadAsArray("dayTwoInput");
         DayTwo totalReports = new DayTwo(dayTwoInputRaw);
         int totalSafeReports = totalReports.getSafeReports();
         System.out.println(totalSafeReports);
@@ -28,17 +28,30 @@ public class Main {
         System.out.println(allowedOneUnsafe);
 
         //////-------------------------------------DAY THREE------------------------------------------------////////
-        inputDownloader.downloadInput("https://adventofcode.com/2024/day/3/input","dayThreeInput");
+        inputDownloader.downloadInput("https://adventofcode.com/2024/day/3/input", "dayThreeInput");
         String dayThreeInputRaw = scanner.loadAsString("dayThreeInput");
         String entireMatchPattern = "mul\\((-?\\d+(\\.\\d+)?),\\s*(-?\\d+(\\.\\d+)?)\\)";
-        int total = DayThree.checkInputStringForMultiplications(entireMatchPattern,dayThreeInputRaw);
-        System.out.println(total);
+        int total = DayThree.checkInputStringForMultiplications(entireMatchPattern, dayThreeInputRaw);
+        System.out.println("Day three part one: "+total);
         String entireMatchPatternSecond = "do\\(\\)|don\\'t\\(\\)|mul\\((-?\\d+(\\.\\d+)?),\\s*(-?\\d+(\\.\\d+)?)\\)";
-        DayThree.findAll(entireMatchPatternSecond,dayThreeInputRaw);
+        int totalTwo = DayThree.findAll(entireMatchPatternSecond, dayThreeInputRaw);
+        System.out.println("Day three part two: "+ totalTwo);
 
-        //
+        //////-------------------------------------DAY FOUR------------------------------------------------////////
+        inputDownloader.downloadInput("https://adventofcode.com/2024/day/4/input", "dayFourInput");
+        ArrayList<String> originalMatrix = scanner.loadAsArray("dayFourInput");
+        //ArrayList<String> originalMatrix = scanner.loadAsArray("dayFourTest");
+        int totalXMASES = 0;
+        totalXMASES+=DayFour.findXMAS(originalMatrix);
+        ArrayList<String> diagnolaisedOriginalMatrix = DayFour.diagnoalise(originalMatrix);
+        totalXMASES+=DayFour.findXMAS(diagnolaisedOriginalMatrix);
+        ArrayList<String> rowsAndColsSwappedMatrix = DayFour.swapRowsAndCols(originalMatrix);
+        totalXMASES+=DayFour.findXMAS(rowsAndColsSwappedMatrix);
+        ArrayList<String> swappedRowsAndColsReversed = DayFour.reverse(rowsAndColsSwappedMatrix);
+        ArrayList<String> diagnolaisedSwappedRowsAndCols = DayFour.diagnoalise(swappedRowsAndColsReversed);
+        totalXMASES+=DayFour.findXMAS(diagnolaisedSwappedRowsAndCols);
+        System.out.println("Day four part one: "+totalXMASES);
 
 
     }
-
 }
