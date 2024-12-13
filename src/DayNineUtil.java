2e6e833a9cb17a87e60ee2dc5f895f47b4ba090d
hmon <wtthumon@gmail.com>
2024-12-13 08:11:20 +0800

    corrected a typo

--- src/DayNineUtil.java
+++ src/DayNineUtil.java
@@ -11,7 +11,7 @@ public class DayNineUtil {
            return value*index;
        }
 
-       public static String replaceaCharInString (String input, int indexOfReplacement, String replacment){
+       public static String replaceCharInString(String input, int indexOfReplacement, String replacment){
            String newString = "";
            for (int i=0; i <input.length(); i++){
                if (i==indexOfReplacement){
