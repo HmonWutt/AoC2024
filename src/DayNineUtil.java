e16f1c24dc2941c01c0eefd515c7803d63c52e0f
hmon <wtthumon@gmail.com>
2024-12-13 08:09:01 +0800

    added method to replace a character at index in string

--- src/DayNineUtil.java
+++ src/DayNineUtil.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class DayNineUtil {
+       public static int getFreeSpaceNeeded(String input,int index){
+           //System.out.println("Index: "+index);
+           return Integer.parseInt(Character.toString(input.charAt(index)));
+       }
+       public static int checkFreeSpaceLeft(int freeSpace, int spaceNeeded) {
+           return freeSpace-spaceNeeded;
+       }
+
+       public static int calculateIndexTimesValue(int value, int index){
+           return value*index;
+       }
+
+       public String replaceaCharInString (String input, int indexOfReplacement, String replacment){
+           String newString = "";
+           for (int i=0; i <input.length(); i++){
+               if (i==indexOfReplacement){
+                   newString+=replacment;
+               }
+               else{
+                   newString+=input.charAt(i);
+               }
+           }
+           return newString;
+       }
+
 }
