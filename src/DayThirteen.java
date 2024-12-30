e3eaa4e2395aeda81541d659a0d1ff55714dc723
hmon <wtthumon@gmail.com>
2024-12-30 18:32:22 +0100

    deleted solve equation class and moved solve method to DayThirteen.java

--- src/DayThirteen.java
+++ src/DayThirteen.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class DayThirteen {
-    public static Integer countTokens(ArrayList<String> input) {
+    public static Integer countTokens(ArrayList<String> input, boolean isPartTwo) {
         Integer totalTokens = 0;
         for (String each : input) {
             ArrayList<Integer> eachSetOfEquations = Regex.getNumbers(each);
@@ -14,9 +14,28 @@ public class DayThirteen {
                     secondEquation.add(eachSetOfEquations.get(i));
                 }
             }
-            if (SolveEquation.solve(firstEquation, secondEquation) > 0)
-                totalTokens += SolveEquation.solve(firstEquation, secondEquation);
+            if (solve(firstEquation, secondEquation) > 0) {
+                totalTokens += (solve(firstEquation, secondEquation));
+            }
         }
         return totalTokens;
+    }
+    public static Integer solve(ArrayList<Integer> first, ArrayList<Integer> second){
+        Integer x = 0;
+        Integer y = 0;
+        Integer x1 = first.get(0) * second.get(0);
+        Integer y1 = first.get(1) * second.get(0);
+        Integer rightSide1 = first.get(2) * second.get(0);
+        Integer y2 = second.get(1) * first.get(0);
+        Integer rightSide2 = second.get(2) * first.get(0);
+        if ((rightSide1-rightSide2)%(y1-y2)  == 0){
+            y = (rightSide1-rightSide2)/(y1-y2);
+            if ((first.get(2) - (first.get(1)*y))% first.get(0) == 0) {
+                x = (first.get(2) - (first.get(1)*y))/ first.get(0);
+                return (x*3) + y;
+            }
+            return -1;
+        }
+        return -1;
     }
 }
