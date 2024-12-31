91d86b6df2ed7bdf18ea93184ba1fbb866ba251a
hmon <wtthumon@gmail.com>
2024-12-31 11:53:15 +0100

    removed redundant method to remove robots  from quadrant

--- src/Quadrant.java
+++ src/Quadrant.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class Board {
+public class Quadrant {
+    private String quadrantNumber;
+    private ArrayList<Robot> robotsInQuadrant;
+    public Quadrant(String name){
+        this.quadrantNumber = name;
+        this.robotsInQuadrant= new ArrayList<>();
+    }
+    public String getQuadrantNumber(){
+        return this.quadrantNumber;
+    }
+    public void addRobot(Robot newRobot){
+        this.robotsInQuadrant.add(newRobot);
+    }
+    public Integer countRobotsInQuadrant(){
+        return this.robotsInQuadrant.size();
+    }
+
 }
