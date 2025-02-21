17cd65155d5525a78a1f80537c5f8f071b3b160e
hmon <wtthumon@gmail.com>
2025-02-21 11:16:16 +0100

    day 10 added part two solution

--- src/Day10.java
+++ src/Day10.java
@@ -1,19 +1,26 @@
+
 import java.util.*;
 
 public class Day10 {
 
-    public static Integer countTrails( ArrayList<String> matrix) {
-        Integer total = 0;
+    public static Integer countTrails( String part, ArrayList<String> matrix) {
+        int total = 0;
         ArrayList<ArrayList<Integer>> trailHeads= findTrailHeads(matrix);
+        int allPathsCount = 0;
         for (ArrayList<Integer> XY: trailHeads ){
-            Set<ArrayList<Integer>> visitedCoordinates = walkTrailHeads(XY.get(0),XY.get(1),matrix);
+            Set<ArrayList<Integer>> visitedCoordinates = walkTrailHeads("one",XY.get(0),XY.get(1),matrix);
+            int pathsCount = walkTrailHeads("two",XY.get(0),XY.get(1),matrix);
             Integer count = countCompleteTrails(visitedCoordinates,matrix);
-            //System.out.println(count+ " trails");
+            allPathsCount+=pathsCount;
             total+=count;
             }
-        return total;
+
+        if (part.equals("one")) return total;
+        return allPathsCount;
+
     }
-    public static Integer countCompleteTrails( Set<ArrayList<Integer>> visitedCoordinates,ArrayList<String>matrix){
+
+    public static Integer countCompleteTrails(Set<ArrayList<Integer>> visitedCoordinates, ArrayList<String>matrix){
         Iterator<ArrayList<Integer>> i = visitedCoordinates.iterator();
         Integer count = 0;
         while (i.hasNext()){
@@ -21,27 +28,31 @@ public class Day10 {
             int x = XY.get(0);
             int y = XY.get(1);
             Integer altitude = getValueAtCoordinate(x,y,matrix);
-            if (altitude == 9) count+=1;
+            if (altitude == 9) {
+                count += 1;
+            }
         }
         return count;
 
     }
-    public static Set<ArrayList<Integer>> walkTrailHeads(Integer x, Integer y,
-                                  ArrayList<String> matrix) {
+    public static <T>T walkTrailHeads(String part,Integer x, Integer y, ArrayList<String> matrix) {
         ArrayList <ArrayList<Integer>> trails = new ArrayList<>();
         Set<ArrayList<Integer>> visitedCoordinates = new HashSet<>();
+        ArrayList<ArrayList<Integer>> paths = new ArrayList<>();
         ArrayList <Integer> altitudeList = new ArrayList<>();
         Integer altitude = getValueAtCoordinate(x,y,matrix);
         Integer length = matrix.size();
         trails.add(new ArrayList<>(Arrays.asList(x,y)));
         visitedCoordinates.add(new ArrayList<>(Arrays.asList(x,y)));
         altitudeList.add(altitude);
-        while (!trails.isEmpty()) {
-            ArrayList<Integer> coordinate = trails.remove(trails.size()-1);
-            altitude = altitudeList.remove(altitudeList.size()-1);
-            x = coordinate.get(0);
-            y = coordinate.get(1);
+        Integer distinctPathCount = 0;
 
+        while (!trails.isEmpty()) {
+            ArrayList<Integer> currentNode= trails.remove(trails.size()-1);
+            altitude = altitudeList.remove(altitudeList.size()-1);
+            x = currentNode.get(0);
+            y = currentNode.get(1);
+            if (altitude == 9) distinctPathCount+=1;
             if (
                 x + 1 < length &&
                 getValueAtCoordinate(x + 1, y, matrix) == altitude + 1 &&
@@ -50,7 +61,10 @@ public class Day10 {
                 trails.add(new ArrayList<>(Arrays.asList(x+1, y)));
                 visitedCoordinates.add(new ArrayList<>(Arrays.asList(x+1,y)));
                 Integer tempAltitude = getValueAtCoordinate(x+1,y,matrix);
+                paths.add(new ArrayList<>(Arrays.asList(x+1,y)));
+
                 altitudeList.add(tempAltitude);
+
             }
             if (x - 1 >= 0 &&
                 getValueAtCoordinate(x - 1, y, matrix) == altitude + 1 &&
@@ -59,6 +73,7 @@ public class Day10 {
                 trails.add(new ArrayList<>(Arrays.asList(x - 1, y)));
                 visitedCoordinates.add(new ArrayList<>(Arrays.asList(x - 1,y)));
                 Integer tempAltitude = getValueAtCoordinate(x-1,y,matrix);
+                paths.add(new ArrayList<>(Arrays.asList(x - 1,y)));
                 altitudeList.add(tempAltitude);
             }
             if (y + 1 < length &&
@@ -68,6 +83,7 @@ public class Day10 {
                 trails.add(new ArrayList<>(Arrays.asList(x, y + 1)));
                 visitedCoordinates.add(new ArrayList<>(Arrays.asList(x,y+1)));
                 Integer tempAltitude = getValueAtCoordinate(x,y+1,matrix);
+                paths.add(new ArrayList<>(Arrays.asList(x,y+1)));
                 altitudeList.add(tempAltitude);
             }
 
@@ -77,13 +93,13 @@ public class Day10 {
                 trails.add(new ArrayList<>(Arrays.asList(x, y - 1)));
                 visitedCoordinates.add(new ArrayList<>(Arrays.asList(x,y-1)));
                 Integer tempAltitude = getValueAtCoordinate(x,y-1,matrix);
+                paths.add(new ArrayList<>(Arrays.asList(x,y-1)));
                 altitudeList.add(tempAltitude);
             }
-
+            if (part.equals("two")) visitedCoordinates.remove(currentNode);
         }
-        return visitedCoordinates;
-
-
+        if (part.equals("two")) return (T) distinctPathCount;
+        return (T) visitedCoordinates;
     }
 
     private static Integer getValueAtCoordinate(Integer x, Integer y, ArrayList<String> matrix) {
@@ -100,6 +116,5 @@ public class Day10 {
         System.out.println("There are: "+trailHeadCoordinates.size()+" trailheads");
         return trailHeadCoordinates;
     }
-
 
 }
